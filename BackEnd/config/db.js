7eb330402f0a1a4b24353150117515e69583f483
Avula Neelakanta <avulaneelakanta75@gmail.com>
2026-04-19 21:51:29 +0530

    Added userSchema using Mongoose library

--- BackEnd/config/db.js
+++ BackEnd/config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 async function connectDB() {
   try {
     await mongoose.connect(process.env.MONGO_URL);
-    console.log("Mongoose connected to MongoDB");
+    console.log("Mongoose connected to MongoDB jaiBabu");
   } catch (error) {
     console.error("Mongoose connection error:", error);
   }
