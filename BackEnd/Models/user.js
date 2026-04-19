9c3592ab6261306887913817de86a5a196931683
Avula Neelakanta <avulaneelakanta75@gmail.com>
2026-04-19 23:54:46 +0530

    Completed thre backend Authentication flow

--- BackEnd/Models/user.js
+++ BackEnd/Models/user.js
@@ -1,1 +1,47 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema; // Define Schema from the mongoose object
+const userSchema = new mongoose.Schema (
+    {
+        username : {
+            type : String,
+            required : [ true, 'Username is required'],
+            unique : true,
+            trim : true,
+            minlength : [4,'Minimum 4 Characters']
+        },
+        age : {
+            type : Number,
+            required : [ true, 'Age is required'],
+            minlength : [1,'Minimum age is 1'],
+            maxlength : [120,'Maximum age is 120']
+        },
+        gender : {
+            type : String,
+            required : [ true, 'Gender is required'],
+            enum: ['male', 'female', 'other'],
+
+        },
+        seatPreference : {
+            type : String,
+            type: String,
+        enum: ['Lower', 'Middle', 'Upper', 'Side Lower', 'Side Upper', 'No Preference'],
+        default: 'No Preference'
+
+        },
+        password : {
+            type : String,
+            required : [ true, 'password is required'],
+
+
+        },
+        email : {
+            type : String,
+            required : [ true, 'email is required'],
+
+
+        }
+    }
+)
+
+const Users = mongoose.model('Users', userSchema);
+module.exports = Users;
--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,12 +3,117 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require("./config/db");
 connectDB();
-const user = require("./models/user")
-
+const User = require("./models/user")
+const Passenger = require("./models/passenger");
 const app = express();
 
 app.use(express.json());
-app.use(cors());
-app.listen(3000,() => {
-    console.log("Server Started");
+app.use(cors("*"));
+app.post("/signup", async (req, res) => {
+    try {
+        const { username, age, gender,password,email } = req.body;
+        if (!username || !age || !gender || !password || !email) {
+           return res.status(400).json({ message: "All Fields are required" });
+        }
+        const user = await User.create({ username, age, gender, email,password})
+        return res.status(201).json({
+         message: 'Account created successfully',
+         user: {
+         username: user.username,
+         age:user.age,
+        
+      }
+    });
+    }
+
+
+    catch (err) {
+        if(err.code === 11000){
+          return  res.json({message:'username already registered'});
+        }
+        return res.status(500).json({ message: err.message });
+
+    }
+
+}
+);
+
+//Login
+app.post("/login",async(req,res) => {
+    try{
+        const {username,password} = req.body;
+        if(!username || !password){
+           return res.status(400).json({message:"username and password are required"});
+
+        }
+        const user = await User.findOne({username})
+        if(!user){
+           return res.status(404).json({message:"Account not found"});
+        }
+        if(user.password !== password){
+           return res.status(401).json({message:"Invalid Login Credentials"});
+        }
+        return res.status(200).json({
+        message: 'Login successful',
+        user: {
+            username: user.username,
+            email: user.email
+        }
+    });
+
+    }
+    catch(err){
+       return res.json({message:err.message})
+    }
+})
+
+//passenger
+
+
+app.post("/passenger", async (req, res) => {
+    try {
+        const {
+            username,
+            age,
+            gender,
+            seatPreference,
+            mealPreference,
+            idProof,
+            idNumber,
+            userId
+        } = req.body;
+
+        // validation
+        if (!username || !age || !gender || !idProof || !idNumber || !userId) {
+            return res.status(400).json({ message: "All required fields must be filled" });
+        }
+
+        const passenger = await Passenger.create({
+            username,
+            age,
+            gender,
+            seatPreference,
+            mealPreference,
+            idProof,
+            idNumber,
+            userId
+        });
+
+        return res.status(201).json({
+            message: "Passenger created successfully",
+            passenger
+        });
+
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+});
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log("Server Started on port 3000");
 })
